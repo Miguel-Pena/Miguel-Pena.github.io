f26843bd7e766811681ecd0960a96d9598d4d16f
Miguel-Pena <68795874+Miguel-Pena@users.noreply.github.com>
2021-06-01 16:03:44 -0500

    Delete javascript directory

--- javascript/showProductCount.js
+++
@@ -1,6 +0,0 @@
-if (localStorage.getItem('cartItems') != null) {
-    
-    cartDetailsArray = JSON.parse(localStorage.getItem('cartDetailsItems'));
-    let counter = document.getElementById('cartCounter');
-    counter.innerHTML = cartDetailsArray.length
-}
